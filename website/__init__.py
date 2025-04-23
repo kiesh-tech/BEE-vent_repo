9a7fd5ded84d75588fb0a669e50b432cddb28e79
Charlene <06kiesha@gmail.com>
2025-04-23 20:56:46 +0800

    files so far, still not working due to jinja error

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path 
+from flask_login import LoginManager
+
+
+
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'goatedd'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    
+
     db.init_app(app)
 
 
@@ -21,12 +28,23 @@ def create_app():
 
     from .models import User, Note
 
+
+    login_manager = LoginManager()  
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    login_manager.user_loader = 'auth.login'
+
     create_database(app)
 
     return app
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database. ')
+    with app.app_context():
+        if not path.exists('website/' + DB_NAME):
+            db.create_all()
+            print('Created Database. ')
 
